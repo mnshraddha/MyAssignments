276c004491339efca7ccd2873a56992d4c7a9090
mnshraddha <104599413+mnshraddha@users.noreply.github.com>
2022-11-08 12:07:57 -0500

    Add files via upload

--- week1/day2/FindSecondLargest.java
+++ week1/day2/FindSecondLargest.java
@@ -7,20 +7,18 @@ public class FindSecondLargest {
 	//Method for sorting array and fining second largest number
 	public  int FindSecondLargestOfArray(int[] data)
 	{
-		int i;
+		
 		Arrays.sort(data);
 		System.out.println("***The sorted Array****");
-		for(i =0;i<data.length;i++)
-		{
-			System.out.println(data[i]);
-		}
+		System.out.println(Arrays.toString(data));
+		
 		return data[data.length-2];
 	}
 
 	public static void main(String[] args) {
 		
 		
-		int data[] = {3,2,11,4,6,7,234,99,113};
+		int data[] = {3,2,11,4,6,7,234,99,113,7777};
 		FindSecondLargest findSecondLargenum = new FindSecondLargest();
 		
 		System.out.println("\nSecond largest number of array is ::"+findSecondLargenum.FindSecondLargestOfArray(data));
